24d81007205a373375fa0fed59b5365c2ca6c4dd
Bosenko Zakhar <2283372ratyha@gmail.com>
2026-03-27 20:57:53 +0200

    feat: create CardComponent and ProjectListComponent

---
+++ src/app/features/projects/project-list/project-list.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-project-list',
+  imports: [],
+  templateUrl: './project-list.html',
+  styleUrl: './project-list.css',
+})
+export class ProjectList {}
